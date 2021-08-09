c194ae0698ff0371e891ff6bdf79085a4b1ce239
EugeneWay <eugeneway@MBP-EugeneWay.home>
2021-08-09 16:34:42 +0100

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+// Import
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+
+async function main() {
+
+    const data = {
+        kind: "i32",
+        value: "10"
+    }
+
+    // Construct
+    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+    const api = await ApiPromise.create({ provider: wsProvider });
+
+    await toHex(data, api);
+}
+
+function toHex (data, api) {
+
+    let res;
+
+    if (data.kind === 'i32') {
+        res = api.createType('Bytes', Array.from(api.createType('i32', data.value).toU8a()));
+    }
+    
+    console.log(res.toHex());
+}
+
+
+main();
